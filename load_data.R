963368175a691b79a5a1bd02399b4e7fef4041e5
JL <pseudohacker@gmail.com>
2025-01-26 16:12:40 +0000

    Se incluye .txt en exclusiones Se define metodología de consula a API con offset

--- load_data.R
+++ load_data.R
@@ -9,16 +9,57 @@ pacman::p_load(dplyr)
 # url <- ("https://api.datosabiertos.mef.gob.pe/DatosAbiertos/v1/datastore_search?resource_id=a9f4cffc-7d0e-4c7c-ab51-1ff115c5beca&limit=5&q=jones")
 
 #version SQL
+#2022-2024
+#por UE
+# query <- paste("https://api.datosabiertos.mef.gob.pe/DatosAbiertos/v1/datastore_search_sql?sql=",
+#                'SELECT * FROM "a9f4cffc-7d0e-4c7c-ab51-1ff115c5beca"',
+#                'WHERE "EJECUTORA_NOMBRE"',
+#                "LIKE 'GOB. REG. MOQUEGUA - HOSPITAL REGIONAL DE MOQUEGUA' OR",
+#                '"EJECUTORA_NOMBRE"',
+#                "LIKE 'REGION MOQUEGUA-SALUD'",
+#                'ORDER BY "KEY_VALUE"')
+# 
+# 
+# url_encoded <- utils::URLencode(query)
+# url <- url_encoded
+# destfile <- here::here("input","input1.txt")
+# curl::curl_download(url, destfile)
+# dt1 <- fromJSON(destfile)$records
+
+
+#por pliego
 query <- paste("https://api.datosabiertos.mef.gob.pe/DatosAbiertos/v1/datastore_search_sql?sql=",
                'SELECT * FROM "a9f4cffc-7d0e-4c7c-ab51-1ff115c5beca"',
-               ' WHERE "SECTOR_NOMBRE" ',
-               "LIKE 'GOBIERNOS REGIONALES' LIMIT 10")
+               'WHERE "PLIEGO_NOMBRE"',
+               "LIKE 'GOBIERNO REGIONAL DEL DEPARTAMENTO DE MOQUEGUA'",
+               'ORDER BY "KEY_VALUE" LIMIT 32000')
+
 url_encoded <- utils::URLencode(query)
 url <- url_encoded
-
-destfile <- here::here("input","input.txt")
+destfile <- here::here("input","pliegoMoqinput1.txt")
 curl::curl_download(url, destfile)
-dt_json <- fromJSON(destfile)
-dt <- dt_json$records
+
+query <- paste("https://api.datosabiertos.mef.gob.pe/DatosAbiertos/v1/datastore_search_sql?sql=",
+               'SELECT * FROM "a9f4cffc-7d0e-4c7c-ab51-1ff115c5beca"',
+               'WHERE "PLIEGO_NOMBRE"',
+               "LIKE 'GOBIERNO REGIONAL DEL DEPARTAMENTO DE MOQUEGUA'",
+               'ORDER BY "KEY_VALUE" LIMIT 32000 OFFSET 32000')
+
+url_encoded <- utils::URLencode(query)
+url <- url_encoded
+destfile <- here::here("input","pliegoMoqinput2.txt")
+curl::curl_download(url, destfile)
+
+#load_data
+files <- dir(here::here("input"), pattern = "^pliegoMoq") # get file names
+dt <- files %>%
+  purrr::map(~ fromJSON(file.path("input", .))$records) %>%
+  purrr::reduce(rbind)
+
+dt %>% 
+  filter(EJECUTORA_NOMBRE == "GOB. REG. MOQUEGUA - HOSPITAL REGIONAL DE MOQUEGUA" |
+           EJECUTORA_NOMBRE == "REGION MOQUEGUA-SALUD") |>
+  group_by(EJECUTORA_NOMBRE) %>% 
+  summarise(N = n(), gasto = sum(as.numeric(MONTO_DEVENGADO_2023)))
 
 
