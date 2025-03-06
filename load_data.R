d01f095060db06d1fbb4c5e2fa1e850e7c1ffded
Javier Loza <javierloza@jlh.work>
2025-03-06 13:53:41 +0000

    v2 Moquegua

--- load_data.R
+++ load_data.R
@@ -10,7 +10,7 @@ pacman::p_load(dplyr)
 #lista de bases
 # 2012-2016 SELECT * FROM "5f3b3cbe-3955-41cc-8662-1757ebb5cf53"
 # 2017-2021 SELECT * FROM "0e2469d8-5872-4bc2-a5bc-91ee01c99df8"
-# 2022-2024 SELECT * FROM "a9f4cffc-7d0e-4c7c-ab51-1ff115c5beca"
+# 2022-2025 SELECT * FROM "a9f4cffc-7d0e-4c7c-ab51-1ff115c5beca"
 
 #por lugar de meta
 counter <- 32000
@@ -19,9 +19,9 @@ while (counter >= 32000) {
   limit <- 32000
   offset <- (i-1)*32000
   query <- glue::glue("https://api.datosabiertos.mef.gob.pe/DatosAbiertos/v1/datastore_search_sql?sql=",
-                      'SELECT * FROM "0e2469d8-5872-4bc2-a5bc-91ee01c99df8"',
-                      'WHERE "DEPARTAMENTO_META_NOMBRE"',
-                      "LIKE 'MOQUEGUA'",
+                      'SELECT * FROM "a9f4cffc-7d0e-4c7c-ab51-1ff115c5beca"',
+                      'WHERE "PLIEGO_NOMBRE"',
+                      "LIKE 'GOBIERNO REGIONAL DEL DEPARTAMENTO DE MOQUEGUA'",
                       'ORDER BY "KEY_VALUE" LIMIT {limit} OFFSET {offset}',.sep = " ")
   
   url_encoded <- utils::URLencode(query)
@@ -34,7 +34,7 @@ while (counter >= 32000) {
 
 #load_data
 files <- dir(here::here("input"), pattern = "^metaMoq") # get file names
-dt_2017_2021 <- files %>%
+dt_2022_2025 <- files %>%
   purrr::map(~ jsonlite::fromJSON(file.path("input", .))$records) %>%
   purrr::reduce(rbind)
 
